d59b374d70a7329f1de0baf733abcd56503144bd
erik van widenfelt <ew2789@gmail.com>
2015-05-21 11:44:20 +0200

    setup

--- setup.py
+++ setup.py
@@ -10,22 +10,18 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='edc-edc_identifier',
-    version='0.0.0-alpha',
+    name='edc-identifier',
+    version='0.0.0a0',
     author=u'Erik van Widenfelt',
     author_email='ew2789@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/botswana-harvard/edc-edc_identifier',
+    url='https://github.com/botswana-harvard/edc-identifier',
     license='GPL license, see LICENSE',
     description='Manage identifiers in the Edc',
     long_description=README,
     zip_safe=False,
     keywords='django contenttype',
-    install_requires=[
-        'Django>=1.7',
-        'unipath>=1.1',
-    ],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
