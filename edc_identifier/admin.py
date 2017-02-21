ce57575c7dec56f153d17b31837dde18f28f2a0d
erikvw <ew2789@gmail.com>
2017-02-22 01:42:31 +0200

    add identifier history to admin

--- edc_identifier/admin.py
+++ edc_identifier/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
 from .admin_site import edc_identifier_admin
-from .models import SubjectIdentifier, IdentifierTracker, Sequence
+from .models import SubjectIdentifier, IdentifierTracker, Sequence, IdentifierHistory
 
 
 @admin.register(SubjectIdentifier, site=edc_identifier_admin)
 class SubjectIdentifierAdmin(admin.ModelAdmin):
 
-    list_display = ('identifier', 'created', 'user_created', 'hostname_created')
+    list_display = (
+        'identifier', 'created', 'user_created', 'hostname_created')
     search_fields = ('identifier', )
     list_filter = ('created', 'user_created')
 
@@ -15,12 +16,25 @@ class SubjectIdentifierAdmin(admin.ModelAdmin):
 @admin.register(IdentifierTracker, site=edc_identifier_admin)
 class IdentifierTrackerAdmin(admin.ModelAdmin):
 
-    list_display = ('identifier', 'root_number', 'counter', 'created', 'user_created')
+    list_display = (
+        'identifier', 'root_number', 'counter', 'created', 'user_created')
     search_fields = ('identifier', 'root_number')
     list_filter = ('created', 'root_number', 'user_created')
 
 
 @admin.register(Sequence, site=edc_identifier_admin)
 class SequenceAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'device_id', 'created', 'user_created', 'hostname_created')
+    list_display = (
+        'pk', 'device_id', 'created', 'user_created', 'hostname_created')
     list_filter = ('created', 'hostname_created', 'user_created', 'device_id',)
+
+
+@admin.register(IdentifierHistory, site=edc_identifier_admin)
+class IdentifierHistoryAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'identifier', 'identifier_type', 'identifier_prefix',
+        'created_datetime', 'created', 'user_created')
+    search_fields = ('identifier', 'identifier_prefix')
+    list_filter = (
+        'created_datetime', 'created', 'identifier_prefix', 'user_created')
