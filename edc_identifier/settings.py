e3e561f83d9f15459583d92df0024eff85132572
erikvw <ew2789@gmail.com>
2017-12-30 15:16:53 +0200

    add migration

--- edc_identifier/settings.py
+++ edc_identifier/settings.py
@@ -16,6 +16,7 @@ import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 APP_NAME = 'edc_identifier'
+ETC_DIR = os.path.join(BASE_DIR, 'etc')
 SITE_ID = 40
 
 # Quick-start development settings - unsuitable for production
