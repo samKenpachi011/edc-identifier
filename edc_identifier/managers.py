8328d905f825a9a2471a9009d50b787af1bfb362
erikvw <ew2789@gmail.com>
2018-01-03 11:39:01 +0200

    add manager for convenience

--- edc_identifier/managers.py
+++ edc_identifier/managers.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+class SubjectIdentifierManager(models.Manager):
+
+    def get_by_natural_key(self, subject_identifier):
+        return self.get(subject_identifier=subject_identifier)
+
+
 class IdentifierManager(models.Manager):
 
     def get_by_natural_key(self, identifier):
